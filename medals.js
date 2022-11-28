d962ff641b03d5984f4cacc72d5a7b9a0c312957
familycomicsstudios <106832853+familycomicsstudios@users.noreply.github.com>
2022-11-28 14:04:41 -0600

    Update medals.js

--- medals.js
+++ medals.js
@@ -12,7 +12,7 @@ class ScratchMedalsExt {
       ]
     };
   }
-  getusers() {
+  async function getusers() {
     return fetch('https://scratchmedals.themadpunter.repl.co/api/v1/users/all', {
     method: 'GET',
     headers: {
