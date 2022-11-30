be9d615bfaa54c69bd6fb3bd90af9873ec9a5c3e
familycomicsstudios <106832853+familycomicsstudios@users.noreply.github.com>
2022-11-30 09:33:31 -0600

    Create savegame.js

---
+++ savegame.js
@@ -0,0 +1,45 @@
+class LocalStorageExtension {
+  getInfo() {
+    return {
+      id: 'localstorageextension', // change this if you make an actual extension!
+      name: 'Save Game',
+      blocks: [
+        {
+          opcode: 'setData',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'Set Item [ONE] to [TWO]',
+          arguments: {
+            ONE: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'level at'
+            },
+            TWO: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: '5'
+            }
+          }
+        },
+        {
+          opcode: 'getData',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'Get Item [ONE]',
+          arguments: {
+            ONE: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'level at'
+            }
+          },
+          disableMonitor: true;
+        }
+      ]
+    };
+  }
+  setData(args) {
+    // Note strict equality: Inputs must match exactly: in type, case, etc.
+    localStorage.setItem(args.ONE, args.TWO);
+  }
+  getData(args) {
+    return localStorage.getItem(args.ONE);
+  }
+}
+Scratch.extensions.register(new StrictEqualityExtension());
